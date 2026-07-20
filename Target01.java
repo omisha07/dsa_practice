ab40ee43503c938edd6ac49668b8095c13c04e78
Omisha <244g1a05r7@srit.ac.in>
2026-07-20 18:23:45 +0530

    sum_target
     To find the target sum of two numbers using arrays

--- Target01.java
+++ Target01.java
@@ -1,25 +1,25 @@
-/**
- * 
- */
-package twopointer;
+package two_pointers;
 
-public class Target01 {
-public static void main(String[] args) {
-	int[] numbers= {2,7,11,15};
-	int target=9;
-	int left=0;
-	int right= numbers.length-1;
-	while(left<right) {
-		int sum= numbers[left]+ numbers[right];
-		if(sum==target) {
-			System.out.println(left + " " + right);
-		 return;
-		}else if(sum > target){
-			right--;
-		}else {
-			left++;
+public class sum_target {
+	public static void main(String[] args) {
+		int[] numbers= {2,7,11,15};
+		int target = 9;
+		int left=0;
+		int right= numbers.length-1;
+		while (left < right) {
+			int sum= numbers[left] + numbers[right];
+			if(sum < target) {
+				left++;
+			}
+			else if(sum>target) {
+				right--;
+			}
+			else {
+				System.out.println(left+ "," +right);
+				break;
+			}
 		}
 	}
-	System.out.println("Not found elements.");
+
 }
-}
+
